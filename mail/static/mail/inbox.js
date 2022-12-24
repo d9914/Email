d1533e2695d7b726738192ba595c74ff28f374fb
d9914 <altierd@sunypoly.edu>
2022-12-24 00:56:18 -0500

    added features

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -13,6 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.querySelector("#compose-form").addEventListener("submit", send_mail);
 
+
+
   // By default, load the inbox
   load_mailbox("inbox");
 });
@@ -21,6 +23,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector("#emails-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "block";
+  document.querySelector("#emails-view").style.display = "none";
 
   // Clear out composition fields
   document.querySelector("#compose-recipients").value = "";
@@ -28,10 +31,51 @@ function compose_email() {
   document.querySelector("#compose-body").value = "";
 }
 
+function reply_email(){
+
+}
+
+function view_mail(email_id) {
+  fetch(`/emails/${email_id}`)
+    .then(response => response.json())
+    .then(email => {
+    
+      document.querySelector("#emails-view").style.display = "none";
+      document.querySelector("#compose-view").style.display = "none";
+      
+      const detailView = document.querySelector("#emails-detail-view");
+      detailView.style.display = "block";
+
+      detailView.innerHTML = "";
+    
+      const detailEmail = document.createElement("div");
+      detailEmail.innerHTML = `
+  <div class="card mb-3">
+    <div class="card-body">
+      <h6 class="card-title font-weight-bold">Sender: ${email.sender}</h6>
+      <h5 class="card-subtitle mb-2">Subject: ${email.subject}</h5>
+      <p class="card-text">Time: ${email.timestamp}</p>
+      <hr class="my-4">
+      <p class="card-text">${email.body}</p>
+      <button id="reply" type="button" class="btn btn-primary">Reply</button>
+    </div>
+  </div>
+`;
+      detailView.append(detailEmail);
+    });
+    fetch(`/emails/${email_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
+      })
+    })
+}
+
 function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
+  document.querySelector("#emails-detail-view").style.display = "none";
 
   // Show the mailbox name
   document.querySelector("#emails-view").innerHTML = `<h3>${
@@ -42,7 +86,6 @@ function load_mailbox(mailbox) {
   .then((response) => response.json())
   .then((emails) => {
 
-    console.log(emails);
     for (const email of emails) {
       const newEmail = document.createElement('div');
       newEmail.className="list-group-item border-dark rounded"
@@ -54,9 +97,9 @@ function load_mailbox(mailbox) {
       newEmail.innerHTML = `<h6>Sender: ${email.sender}</h6> 
         <h5>Subject: ${email.subject} </h5>
         <p>Time: ${email.timestamp} </p>`;
-        newEmail.addEventListener('click', function() {
-          console.log('This element has been clicked!')
-      });
+        newEmail.addEventListener('click', function(){
+          view_mail(email.id);
+        }); 
       document.querySelector('#emails-view').append(newEmail);
     }
   });
